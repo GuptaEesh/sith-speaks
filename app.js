35fd420cb189206f7592281577cfbaba8b1bffca
GuptaEesh <p1r8a5t7ibha@gmail.com>
2021-09-11 15:29:33 +0530

    feat: made fully

---
+++ app.js
@@ -0,0 +1,23 @@
+var Input = document.querySelector("#txt-input");
+var press = document.querySelector("#btn");
+var Output = document.querySelector("#txt-output");
+
+var mainURL = "	https://api.funtranslations.com/translate/sith.json";
+
+function translateFull(input) {
+    return mainURL + "?" + "text=" + input;
+}
+
+function errorHandler(error) {
+    console.log("Error Occured", error);
+    alert("Server is not responding, pls try again!");
+}
+
+function clickHandler() {
+    var inputText = Input.value;
+    fetch(translateFull(inputText)).then(response => response.json()).then(json => {
+        var transforM=json.contents.translated;Output.innerText = transforM;
+    }).catch(errorHandler);
+
+};
+press.addEventListener("click", clickHandler);
